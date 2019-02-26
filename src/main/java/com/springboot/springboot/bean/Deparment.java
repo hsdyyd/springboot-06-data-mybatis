802b73b9c52e65785c8a073d217cacc7cc3445e5
hsdyyd <54978101@qq.com>
2019-02-26 10:41:46 +0800

    springboot整合mybatis准备

---
+++ src/main/java/com/springboot/springboot/bean/Deparment.java
@@ -0,0 +1,26 @@
+package com.springboot.springboot.bean;
+
+/**
+ * @author yidong
+ * @create 2019-02-26-10:39
+ */
+public class Deparment {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/springboot/springboot/bean/Employee.java
@@ -0,0 +1,53 @@
+package com.springboot.springboot.bean;
+
+/**
+ * @author yidong
+ * @create 2019-02-26-10:38
+ */
+public class Employee {
+    private Integer id;
+    private String name;
+    private Float salary;
+    private String email;
+    private Integer dept_no;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Float salary) {
+        this.salary = salary;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getDept_no() {
+        return dept_no;
+    }
+
+    public void setDept_no(Integer dept_no) {
+        this.dept_no = dept_no;
+    }
+}
